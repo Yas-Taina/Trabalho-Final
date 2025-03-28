160c154fe0385db1bcaca8bdeed3867b1609b605
Mateus De Vita <mateusdevitata@gmail.com>
2025-03-27 23:12:44 -0300

    Demonstração simples

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
+import { CepService } from './services/cep/cep.service';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,18 @@ import { MenuComponent } from './components/menu/menu.component';
 })
 export class AppComponent {
   title = 'Trabalho-Final';
+
+  constructor(cepService: CepService) {
+    cepService.ObterEndereco('01001-000').subscribe({
+      next: (endereco) => {
+        console.log(endereco);
+      },
+      error: (error) => {
+        console.error('Erro ao obter endereço:', error);
+      },
+      complete: () => {
+        console.log('Requisição concluída');
+      }
+    });
+  }
 }
