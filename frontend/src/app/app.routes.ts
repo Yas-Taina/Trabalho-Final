c9fcb880199ef259926476a9f284c83d40afc88f
Christian-Eurinidio <113392481+Christian-Eurinidio@users.noreply.github.com>
2025-04-03 20:15:48 -0300

    feat<cadastro> Adiciona tela de cadastro com redirecionamento para tela de login (#5)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './components/telas/login/login.component';
+import { HomeComponent } from './components/telas/home/home.component';
+import { CadastroComponent } from './components/telas/cadastro/cadastro.component';
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'cadastro', component: CadastroComponent },
+    { path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
