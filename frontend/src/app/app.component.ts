d199bf69e4d749135d7ced2ec391df98fa27aa8e
Qtaro-Kujo <joao_albertop@hotmail.com>
2025-04-04 14:56:49 -0300

    feat: Criacao de componente reutilizavel para footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { CepService } from './services/cep/cep.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent, RouterModule],
+  imports: [CommonModule, RouterOutlet, MenuComponent, FooterComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
