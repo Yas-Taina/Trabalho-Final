22d78c301dff517679b56b0f3a3137308abddbb0
Mateus De Vita <mateusdevitata@gmail.com>
2025-04-03 22:47:13 -0300

    feat(utilitários): implementa messagebox básico

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,30 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CepService } from './services/cep/cep.service';
+import { ModalComponent } from "./components/utilitarios/modal/modal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent, FooterComponent, RouterModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MenuComponent,
+    RouterModule,
+    FooterComponent,
+    ModalComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild(ModalComponent) modalComponent!: ModalComponent;
   title = 'Trabalho-Final';
   CepService: CepService;
 
   constructor(cepService: CepService) {
     this.CepService = cepService;
-
-    this.carregarEndereco();
   }
 
   async carregarEndereco() {
     const endereco = await this.CepService.ObterEndereco('01001-000');
 
     console.log(endereco);
+
+    this.modalComponent.open(JSON.stringify(endereco));
   }
 }
