310140ace7e20a9208168f418ed71410d4f739e7
Yas Tainá <yas.taina.silva@gmail.com>
2025-04-06 14:17:54 -0300

    feat(RF017): CRUD completo Equipamentos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { HomeComponent } from './pages/public/home/home.component';
 import { CadastroComponent } from './pages/auth/cadastro/cadastro.component';
 import { ClienteInicioComponent } from './pages/cliente/inicio/inicio.component';
 import { EmpregadoInicioComponent } from './pages/empregado/inicio/inicio.component';
-import { ListarEquipamentoComponent } from './pages/empregado/listar-equipamento/listar-equipamento.component';
-import { InserirEquipamentoComponent } from './pages/empregado/inserir-equipamento/inserir-equipamento.component';
+import { ListarEquipamentoComponent } from './pages/empregado/equipamento/listar-equipamento/listar-equipamento.component';
+import { InserirEquipamentoComponent } from './pages/empregado/equipamento/inserir-equipamento/inserir-equipamento.component';
+import { EditarEquipamentoComponent } from './pages/empregado/equipamento/editar-equipamento/editar-equipamento.component';
 
 export const routes: Routes = [
     { path: 'public/home', component: HomeComponent },
@@ -13,7 +14,8 @@ export const routes: Routes = [
     { path: 'auth/cadastro', component: CadastroComponent },
     { path: 'client/home', component: ClienteInicioComponent },
     { path: 'adm/home', component: EmpregadoInicioComponent },
-    { path: 'adm/equipamentos/listar', component: ListarEquipamentoComponent},
+    { path: 'adm/equipamentos', component: ListarEquipamentoComponent},
     { path: 'adm/equipamentos/novo', component: InserirEquipamentoComponent},
+    { path: 'adm/equipamentos/editar/:id', component: EditarEquipamentoComponent},
     { path: '',   redirectTo: '/public/home', pathMatch: 'full' }
 ];
--- frontend/src/app/pages/empregado/equipamento/listar-equipamento/listar-equipamento.component.ts
+++ frontend/src/app/pages/empregado/equipamento/listar-equipamento/listar-equipamento.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { EquipamentoService } from '../../../services/equipamento.service';
-import { Equipamento } from '../../../shared/models/equipamento.model';
+import { EquipamentoService } from '../../../../services/equipamento.service';
+import { Equipamento } from '../../../../shared/models/equipamento.model';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -21,6 +21,13 @@ export class ListarEquipamentoComponent {
   }
   listarTodos(): Equipamento[] {
     return this.equipamentoService.listarTodos();
+  }
+  remover($event: any, equipamento: Equipamento): void {
+    $event.preventDefault();
+    if (confirm('Deseja deletar este equipamento? Esta ação não pode ser revertida')){
+      this.equipamentoService.remover(equipamento.id!);
+      this.equipamentos = this.listarTodos();
+    }
   }
 
 }
