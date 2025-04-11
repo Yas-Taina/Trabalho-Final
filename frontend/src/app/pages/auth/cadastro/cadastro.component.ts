b23cc30d2d567e2b996efce58454a052481a1e10
Mateus De Vita <mateusdevitata@gmail.com>
2025-04-10 22:37:43 -0300

    feat(autocadastro): Vincula service no componente

--- frontend/src/app/pages/auth/cadastro/cadastro.component.ts
+++ frontend/src/app/pages/auth/cadastro/cadastro.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { Cliente } from '../../../shared/models/cliente.model';
+import { ClienteService } from '../../../services/cliente.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,5 +12,19 @@ import { RouterModule } from '@angular/router';
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  @ViewChild('formCliente') formCliente!: NgForm;
+  cliente: Cliente = new Cliente();
+  confirmarsenhamodel = '';
 
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router
+  ) { }
+
+  inserir(): void {
+    if (this.formCliente.form.valid) {
+      this.clienteService.inserir(this.cliente);
+      this.router.navigate(['/client/home']);
+    }
+  }
 }
