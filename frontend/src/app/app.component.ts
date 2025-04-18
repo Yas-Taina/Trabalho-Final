661d2fe3fc6a1c930cefb4dc24e75654fa7e8e4f
Mateus De Vita <mateusdevitata@gmail.com>
2025-04-18 16:03:55 -0300

    chore(código): padroniza formatação em alguns arquivos

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,15 +5,15 @@ import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent, FooterComponent, RouterModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, MenuComponent, FooterComponent, RouterModule],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Trabalho-Final';
+    title = 'Trabalho-Final';
 
-  constructor() {
-  }
+    constructor() {
+    }
 }
