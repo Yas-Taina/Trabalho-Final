45aae1e5d5da41e9a122ff603896c3783b2a17c1
Yas Tainá <yas.taina.silva@gmail.com>
2025-04-19 18:10:51 -0300

    feat(rf012): Adiciona service e model de solicitação

---
+++ frontend/src/app/services/orcamento.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Orcamento } from '../shared/models/orcamento.model';
+import { ServiceCrudBase } from './service-crud-base/service-crud-base';
+
+const LS_CHAVE = "orcamentos";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class OrcamentoService extends ServiceCrudBase<Orcamento> {
+
+  constructor() {
+    super(LS_CHAVE);
+  }
+
+}
