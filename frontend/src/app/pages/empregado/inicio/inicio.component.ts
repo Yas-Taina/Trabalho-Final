d4ce1e1d6eb36736e9ce156937a8971ede36ce0d
MateusDV <MateusDV@users.noreply.github.com>
2025-04-21 21:05:18 -0300

    feat(relatorios): implementa simulação da geração de relatórios de receita (#25)

--- frontend/src/app/pages/empregado/inicio/inicio.component.ts
+++ frontend/src/app/pages/empregado/inicio/inicio.component.ts
@@ -3,6 +3,8 @@ import { SolicitacaoService } from "../../../services/solicitacao.service";
 import { Solicitacao } from "../../../shared/models/solicitacao.model";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { ReceitasComponent } from "../relatorios/receitas/receitas.component";
 
 @Component({
   selector: "app-inicio",
@@ -14,12 +16,26 @@ import { RouterModule } from "@angular/router";
 export class EmpregadoInicioComponent {
   solicitacoes: Solicitacao[] = [];
 
-  constructor(private solicitacaoService: SolicitacaoService) {}
+  constructor(
+    private solicitacaoService: SolicitacaoService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.solicitacoes = this.listarTodos();
   }
+
   listarTodos(): Solicitacao[] {
     return this.solicitacaoService.listarTodos();
+  }
+
+  abrirModalRelatorioReceitas(){
+    this.modalService.open(ReceitasComponent);
+  }
+
+  gerarRelatorioReceitasPorCategoria() {
+    // Abrir pdf arbitrário em nova aba
+    const pdfUrl = "/assets/files/relatorio_exemplo.pdf";
+    window.open(pdfUrl, '_blank');
   }
 }
