770a418d7edbf7d29877fa0e88f6570771627a06
Qtaro-Kujo <joao_albertop@hotmail.com>
2025-06-05 22:49:08 -0300

    Chore: Inicializa projeto spring boot

---
+++ backend/src/test/java/br/net/trabalho_final/trabalho_final/TrabalhoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.trabalho_final.trabalho_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
