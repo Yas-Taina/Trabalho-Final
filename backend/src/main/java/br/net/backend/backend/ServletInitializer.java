7e134cd5ee3215bc0f8f55cd866b59b19292c821
Christian-Eurinidio <113392481+Christian-Eurinidio@users.noreply.github.com>
2025-06-06 21:02:54 -0300

    <chor>Implementa backend

--- backend/src/main/java/br/net/backend/backend/ServletInitializer.java
+++ backend/src/main/java/br/net/backend/backend/ServletInitializer.java
@@ -1,4 +1,4 @@
-package br.net.trabalho_final.trabalho_final;
+package br.net.backend.backend;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TrabalhoFinalApplication.class);
+		return application.sources(BackendApplication.class);
 	}
 
 }
---
+++ backend/src/main/java/br/net/backend/backend/rest/UsuarioREST.java
@@ -0,0 +1,30 @@
+package br.net.backend.backend.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.net.backend.backend.model.Usuario;
+
+@CrossOrigin
+@RestController
+public class UsuarioREST {
+
+    public static List<Usuario> lista = new ArrayList<>();
+
+    @GetMapping("/usuarios")
+    public List<Usuario> obterTodosUsuarios() {
+        return lista;
+    }
+
+    static {
+        lista.add(new Usuario(1, "administr", "admin", "admin", "ADMIN"));
+        lista.add(new Usuario(2, "gerent", "gerente", "gerente", "GERENTE"));
+        lista.add(new Usuario(3, "funcion", "func", "func", "FUNC"));
+    }
+
+    
+}
