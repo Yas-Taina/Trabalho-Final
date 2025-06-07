4611908622973e33f6b4bc3ef86c2ec69fd882da
Christian-Eurinidio <113392481+Christian-Eurinidio@users.noreply.github.com>
2025-06-06 21:03:36 -0300

    <chor>Implementa backend (#49)

--- backend/src/main/java/br/net/backend/backend/BackendApplication.java
+++ backend/src/main/java/br/net/backend/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package br.net.trabalho_final.trabalho_final;
+package br.net.backend.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrabalhoFinalApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrabalhoFinalApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
